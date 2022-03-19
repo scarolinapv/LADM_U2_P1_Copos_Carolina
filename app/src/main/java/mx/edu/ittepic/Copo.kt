21172abb6612a7d669f42e9306daad87d96a9b75
Carolina Plata <90290718+scarolinapv@users.noreply.github.com>
2022-03-19 10:05:35 -0700

    testing

---
+++ app/src/main/java/mx/edu/ittepic/Copo.kt
@@ -0,0 +1,27 @@
+package mx.edu.ittepic
+
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+
+class Copo {
+    var x = 0f
+    var y = 0f
+    var tam = 0f
+
+    init{
+        x = (Math.random()*2000).toFloat()
+        y = ((Math.random()*2500)*-1).toFloat()
+        tam = ((Math.random()*5)+5).toFloat()
+    }
+    fun moverCopo(){
+        y += tam
+        if(y>1000) y= ((Math.random()*2500)*-1).toFloat()
+    }
+    fun pintarCopo(c: Canvas){
+        val p = Paint()
+        p.color = Color.rgb(245,225,206)
+        c.drawCircle(x,y,tam,p)
+    }
+
+}
